3f635626eb565db2fe80499c2f05734641dd0960
Lisery <1143765064@qq.com>
2024-02-04 15:39:56 +0800

    修复右上角按钮边缘无法改变窗口大小的bug

---
+++ ECGPlatform/Utils/Converters/WindowStateToControlButtonMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace ECGPlatform;
+
+[ValueConversion(typeof(WindowState), typeof(Thickness))]
+public class WindowStateToControlButtonMarginConverter : IValueConverter
+{
+    public static WindowStateToControlButtonMarginConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var resizeBorderSize = (double)Application.Current.FindResource("ResizeBorderSize")!;
+
+        var state = (WindowState)value;
+        return state switch
+        {
+            WindowState.Normal => new Thickness(0,resizeBorderSize,0,0),
+            WindowState.Minimized => new Thickness(0,resizeBorderSize,0,0),
+            WindowState.Maximized => new Thickness(0),
+            _ => throw new ArgumentOutOfRangeException(nameof(state)),
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
